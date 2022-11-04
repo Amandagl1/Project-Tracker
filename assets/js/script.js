00e645153fc47f2bb2060e8c987afc6b04cc6685
Amanda Lee <leeandama87@gmail.com>
2022-11-04 13:03:46 -0700

    Create modal with form inside. Asks user to input project name, project type, hourly wage for project and the due date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-// Display current time and date using setInterval() and Moment.js for formatting
-var timeNow = moment().format("MMMM Do, YYYY");
-$("#time").text(timeNow);
-var dateNow = moment().format("Today: dddd");
-$("#date").text(dateNow);
+// Selects the div ID that will set the current time and date
+var displayClock = $("#clock")
+
+function updateTime() {
+    displayClock.html(moment().format("dddd, MMMM Do YYYY, hh:mm:ssA"));
+}
+
+setInterval(updateTime, 1000);
+
