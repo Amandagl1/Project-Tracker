a3ae963fb9f747420f7bcd5c71b5ea511df27080
Amanda Lee <leeandama87@gmail.com>
2022-11-20 12:27:46 -0800

    Added local storage to js file, and index.html.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Project-Tracker
 
 # Description
+- This is a webpage that allows you to track your projects. It is a work day scheduler where you can input information about your project such as the projec title, the type of project it is, the hourly wage, and the deadline.
+- This project benefits users keep an organized and timely plan for their day. Users can input information into the scheduler by the hour and allows them to save the information in the local server.
 
 # Screenshots
 ![Screenshot of Project Tracker Webpage](images/Project-tracker.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,40 @@
 var displayClock = $("#clock")
 
 function updateTime() {
+    // Moment.js for display format of time
     displayClock.html(moment().format("dddd, MMMM Do YYYY, hh:mm:ssA"));
 }
 
+// Updates every second
 setInterval(updateTime, 1000);
 
-var resetData = document.getElementById("cancel-btn")
 
-function cancelRequest() {
-    resetData.reset();
-};
+// Using jQuery to set local storage
+$(document).ready(function() {
+    $(".save-btn").click(function() {
+        var input = $(this).siblings(".user-input").val();
+        var user = $(this).parent().attr("class");
 
-resetData.addEventListener("onClick", cancelRequest);
+    // Setting item to local storage
+    localStorage.setItem(user, input);
+    // Logs user input in the console
+    console.log(input);
+
+    });
+});
+
+// Getting item from local storage
+$(".user-input").val(localStorage.getItem(".user-input"));
 
 
-var formData = document.getElementById("form");
-form.addEventListener("submit", submitForm);
-localStorage.setItem("form", formData.value);
-
-var getForm = localStorage.getItem("form");
-
-function submitForm(e) {
-    e.preventDefault();
+window.onload = function() {
+    localStorage.getItem("9:00am", $('#9am').val());
+    localStorage.getItem("10:00am", $('#10am').val());
+    localStorage.getItem("11:00am", $('#11am').val());
+    localStorage.getItem("12:00pm", $('#12pm').val());
+    localStorage.getItem("1:00pm", $('#1pm').val());
+    localStorage.getItem("2:00pm", $('#2pm').val());
+    localStorage.getItem("3:00pm", $('#3pm').val());
+    localStorage.getItem("4:00pm", $('#4pm').val());
+    localStorage.getItem("5:00pm", $('#5pm').val());
 }
-
